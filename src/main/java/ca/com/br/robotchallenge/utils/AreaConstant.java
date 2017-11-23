d790414678fb841eb865089d32e34ee1fe25f784
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-11-23 01:17:12 -0200

    Adiciona classes utils e constantes

---
+++ src/main/java/ca/com/br/robotchallenge/utils/AreaConstant.java
@@ -0,0 +1,10 @@
+package ca.com.br.robotchallenge.utils;
+
+public class AreaConstant {
+    private AreaConstant() {}
+
+    public static final int MAX_X = 5;
+    public static final int MIN_X = 0;
+    public static final int MAX_Y = 5;
+    public static final int MIN_Y = 0;
+}
---
+++ src/main/java/ca/com/br/robotchallenge/utils/OrientationEnum.java
@@ -0,0 +1,5 @@
+package ca.com.br.robotchallenge.utils;
+
+public enum OrientationEnum {
+    NORTH, SOUTH, EAST, WEST;
+}
