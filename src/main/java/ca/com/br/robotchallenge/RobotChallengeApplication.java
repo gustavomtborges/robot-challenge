e8a7a4047568a38659b813ad322a379f5b598350
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2017-11-23 01:14:11 -0200

    Cria metodo main para aplicacao Spring Boot

--- src/main/java/ca/com/br/robotchallenge/RobotChallengeApplication.java
+++ src/main/java/ca/com/br/robotchallenge/RobotChallengeApplication.java
@@ -2,23 +2,12 @@ package ca.com.br.robotchallenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RobotChallengeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RobotChallengeApplication.class, args);
-	}
-}
-
-@RestController
-class MainController {
-
-	@RequestMapping("/v1/api")
-	public String index() {
-		return "Robot Challange API";
 	}
 }
 
